71cee602f69d87fee07035780e653594fa608c5e
Sam Place <samvauclain@gmail.com>
2021-11-28 15:17:30 -0800

    testing player & potions

---
+++ __tests__/Player.test.js
@@ -0,0 +1,20 @@
+// When requiring modules, it's not necessary to include the .js file extension. Node will assume that the file is a JS file if no extension is specified.
+const Player = require('../lib/Player');
+
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+  });
